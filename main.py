51d1d06e5a5ce49a651ae82374e7e4c4978e7f71
Terbik <terbik44@gmail.com>
2023-05-17 22:34:34 +0300

    'next'

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import math
+from re import match
+
+
 def transfer_t_to_mui():
 
     ''' перевод t в mui '''
 
-    question = int(input('У нас известно интенсивность?(1 - да; 0 - нет) - '))
+    # question = int(input('У нас известно интенсивность?(1 - да; 0 - нет) - '))
+
+    question = 1
     if question == 1:
-        mui = float(input('Введите интенсивность потока обслуживания - '))
-        print(f'mui = {mui}')
-        return mui
+        # mui = float(input('Введите интенсивность потока обслуживания - '))
+        mui = 0.33
+        print(f'\nmui = {mui}\n')
+
     else:
         t = int(input('Введите время обслуживания - '))
         mui = float(1/t)
-        print(f'mui = {mui}')
-        return mui
+        print(f'\nmui = {mui}\n')
+
+    lambda_question = input('Введите ламбду и тип времени (час, мин, сут) ПРИМЕР (30 сут) --- ')
+    lambda_, type_lambda = lambda_question.split(' ')
+    float(lambda_)
+
+    match type_lambda:
+        case 'час':
+            lambda_ /= 60
+        case 'сут':
+            lambda_ = lambda_ / 24
+            print(lambda_)
+            lambda_ = lambda_ / 60
+            print(lambda_)
+
+
 
 
 def tao_processing(n, mui, lambda_):
     one = n * mui
     tao = lambda_ / one
-    print(f'tao = {tao}')
+    print(f'tao = {tao}\n')
     return tao
 
 
@@ -49,10 +69,48 @@ def limit_processing(n, m, tao):
             all_iteration[f'p_{k}'] = two * tao**k * p_0
 
     for keys, values in all_iteration.items():
-        print(keys)
-        print(values)
+        print(keys, '=', values, '\n')
+
+    return all_iteration
 
 
+def relative_and_absolute(lambda_, all_iteration, n, m):
+
+    Q = 1 - all_iteration[f'p_{n + m}']
+    print(f'Q = {Q}\n')
+
+    A = lambda_ * Q
+    print(f'A = {A}\n')
+
+    return A
+
+def all_N_and_T(A, mui, tao, m, n, all_iteration, lambda_):
+
+    N_s = A / mui
+    print(f'N_s = {N_s}\n')
+
+    one_N_line = n**n / math.factorial(n)
+    two_N_line = one_N_line * tao**n+1
+    some = 1 - tao**m
+    three_N_line = some * (m + 1 - m * tao)
+    four_N_line = 1 - tao
+    five = three_N_line / four_N_line**2
+
+    N_line = two_N_line * five * all_iteration['p_0']
+    print(f'N_line = {N_line}\n')
+
+    N = N_s + N_line
+    print(f'N = {N}\n')
+
+
+    T_s = N_s / lambda_
+    print(f'T_s = {T_s}\n')
+
+    T_line = N_line / lambda_
+    print(f'T_line = {T_line}\n')
+
+    T = T_s + T_line
+    print(f'T = {T}\n')
 
 
 def main():
@@ -65,7 +123,9 @@ def main():
     lambda_ = 1.25
     mui = transfer_t_to_mui()
     tao = tao_processing(n, mui, lambda_)
-    p_0 = limit_processing(n, m, tao)
+    all_iteration = limit_processing(n, m, tao)
+    A = relative_and_absolute(lambda_, all_iteration, n, m)
+    all_N_and_T(A, mui, tao, m, n, all_iteration, lambda_)
 
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -1,36 +1,43 @@
-import math
+# import math
+#
+# count = 0
+# total = 0
+# n = 3
+# m = 5
+# tao = 1.262
+#
+# count = 0
+# total = 0
+# for count in range(n):
+#     total += n ** count / math.factorial(count) * tao ** count
+#
+# factorial = math.factorial(n)
+# first = n ** n / factorial
+# second = tao ** n + 1
+# third = 1 - tao ** m
+# fourth = 1 - tao
+# test_ = first * second * third
+# test_2 = test_ / fourth
+# p_0 = (total + test_2) ** -1
+#
+# while count <= n:
+#     total += (n ** count / math.factorial(count)) * (tao ** count)
+#     count += 1
+#
+# factorial = math.factorial(n)
+# first = n ** n / factorial
+# second = tao ** n + 1
+# third = 1 - tao ** m
+# fourth = 1 - tao
+# test_ = first * second * third
+# test_2 = test_ / fourth
+# p_0 = (total + test_2) ** -1
 
-count = 0
-total = 0
-n = 3
-m = 5
-tao = 1.262
+lambda_quastion = input('Введите ламбду и тип времени (час, мин, сут) ПРИМЕР (30 сут) --- ')
+lambda_, type_lambda = lambda_quastion.split(' ')
+lambda_ = float(lambda_)
 
-count = 0
-total = 0
-for count in range(n):
-    total += n ** count / math.factorial(count) * tao ** count
-
-factorial = math.factorial(n)
-first = n ** n / factorial
-second = tao ** n + 1
-third = 1 - tao ** m
-fourth = 1 - tao
-test_ = first * second * third
-test_2 = test_ / fourth
-p_0 = (total + test_2) ** -1
-
-while count <= n:
-    total += (n ** count / math.factorial(count)) * (tao ** count)
-    count += 1
-
-factorial = math.factorial(n)
-first = n ** n / factorial
-second = tao ** n + 1
-third = 1 - tao ** m
-fourth = 1 - tao
-test_ = first * second * third
-test_2 = test_ / fourth
-p_0 = (total + test_2) ** -1
-
-print(p_0)
+lambda_ = lambda_ / 24
+print(lambda_)
+lambda_ = lambda_ / 60
+print(lambda_)
